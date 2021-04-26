0904cf5ed182e15c6e7701460e9b8ec85bdcac36
Soyeon Park <sunho2107@naver.com>
2021-04-26 11:13:38 +0900

    11

---
+++ 0426/0426 ch05 dplyr.R
@@ -0,0 +1,56 @@
+# dplyr 라이브러리를 이용한 데이터 가공
+# 우리가 알아야 할 dplyr 함수들
+# filter, select, arrange, summarize(group_by) , mutate
+# 행 추출, 열 추출, 정렬(오름차(ascending, default값), 내림차(descending)), 데이터를 변수별로 묶어서 연산값을 출력 , 데이터를 새로운 열로 생성.
+# %>% (함수를 가독성 있고 편하게 쓰기 위한 것. 다른 함수와 이어 줄 수 있어 직관적으로 코드를 짤 수 있다.)
+
+library(dplyr)
+library(gapminder)
+
+# 1. select - 원하는 열을 추출.
+select(gapminder, country, year, lifeExp)
+select(gapminder, country, year, lifeExp) %>% head()
+select(gapminder, country, year, lifeExp) %>% head(10)
+
+# filter - 원하는 행을 추출.
+filter(gapminder, country == 'Croatia')
+filter(gapminder, country == 'Croatia' & year >= 2000)
+filter(gapminder, continent == 'Europe' & year == 2007)
+
+# 3. arrange - 정렬, 디폴트는 오름차순.(asc)
+europe_pop <- filter(gapminder, continent == 'Europe' & year == 2007)
+arrange(europe_pop, lifeExp)
+arrange(europe_pop, desc(lifeExp))
+# 아프리카 대륙에서 2007년 평균 수명 상위 5개국을 구하여라.
+africa_pop <- filter(gapminder, continent == 'Africa' & year == 2007)
+arrange(africa_pop, desc(lifeExp)) %>% head(5)
+
+# 파이프로 따로 변수지정 안 하고, 한 줄로 줄일 수 있음.
+filter(gapminder, continent == 'Africa' & year == 2007) %>% arrange(desc(lifeExp)) %>% head(5)
+gapminder %>% filter(continent == 'Africa' & year == 2007) %>% arrange(desc(lifeExp)) %>% head(5) # 가장 많이 쓰는 패턴.
+
+# 4. group_by 와 summarize
+summarize(africa_pop, pop_avg=mean(pop))                        # 2007년 아프리카 국가별 평균 인구 수 
+summarize(group_by(gapminder, continent), pop_avg=mean(pop))    # 대륙별 인구평균(*얘는 연도 지정은 안 된 것!)
+summarize(group_by(gapminder, country), life_avg=mean(lifeExp)) # 나라별 평균 기대수명
+
+asia_pop <- gapminder %>%
+    filter(continent == 'Asia')
+summarize(group_by(asia_pop, country), life_avg=mean(lifeExp)) %>%
+    arrange(desc(life_avg))%>%
+    head(5)
+
+#연습
+#mag(mile-per-gallon) 데이터, ggplot2 라이브러리 사용.
+
+library(ggplot2)
+head(mpg)
+?mpg
+
+glimpse(mpg)               # 데이터 훑어보기
+summary(mpg)               # 요약 통계량
+
+df <- mpg
+
+
+
--- 0426/0426 ch05 데이터 가공.R
+++ 0426/0426 ch05 데이터 가공.R
@@ -5,8 +5,8 @@ library(gapminder)
 library(dplyr)
 
 
-glimpse(gapminder)              # glimpse는 dplyr 패키지 함수(데이터 tibble 형태로 훑어보기)
-                                # tibble (컬럼 이름, 속성(요소), 타입 나오는 것.)
+glimpse(gapminder)              # glimpse는 dplyr 패키지 함수(데이터 훑어보기)
+                                # tibble (컬럼 이름, 속성, 타입 나오는 것.)
 
 # 각 나라의 기대 수명(lifeExp)
 tail(gapminder[, c('country', 'lifeExp')])
@@ -24,7 +24,26 @@ gapminder[gapminder$country == 'Croatia', c('year', 'pop')]
 
 gapminder[gapminder$country == 'Croatia' & gapminder$year >= 1990 , c('year', 'lifeExp', 'pop')]
 
+# 행/열 단위의 연산
+# base R에서는 apply 함수로 했었음.
+apply(gapminder[gapminder$country == 'Croatia', c('lifeExp', 'pop')], 2, mean)
 
+# 연평균 증가율 구해보기.(책에 없는 내용임.)
+# A-B-C로 변했다고 했을 때, CAGR = ((C셀/A셀)^(1/n))-1
+# 예를 들어 10000 -> 20000 기간이 10년 동안 변화했다고 했을 때 연평균 증가율은
+2**0.1 - 1
+# 복리의 개념과 같다. 만약 이자율이 0.718 이면 10년 뒤에 원금은 2배가 됨.
 
+# 크로아티아의 연평균 인구 증가율(데이터는 크로아티아 인구, 연도 추출한 tibble에서 보면 됨.)
+(4493312/3882229)^(1/55) - 1
+
+# 연평균 증가율을 구하는 함수를 만들어 보자.
+
+# 사용자 정의 함수 만들어 apply 함수에 적용해 보기. (단, 변수가 하나인 함수.)
+peak2peak = function(x) {
+    return(max(x) - min(x))
+}
+
+apply(gapminder[gapminder$country == 'Croatia', c('lifeExp', 'pop')], 2, peak2peak)
 
 
